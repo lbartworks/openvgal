b6993bf39a14c956574e8074384e6bd29b9fb78f
LB artworks <lbartworks@protonmail.com>
2023-05-13 00:28:45 +0200

    updates for version 0.3

--- hall_builder/room_builder_aux.js
+++ hall_builder/room_builder_aux.js
@@ -9,6 +9,99 @@ async function  doDownload(filename, scene) {
 }
 
 
+var item_builder= function(name, item_position, item_size, vector, material,scene){
+	var abstractPlane = BABYLON.Plane.FromPositionAndNormal(new BABYLON.Vector3(0, 0, 0),vector );
+	var item = BABYLON.MeshBuilder.CreatePlane(name, {sourcePlane: abstractPlane, width:item_size.width, height: item_size.height, sideOrientation: BABYLON.Mesh.SINGLESIDE},scene);
+	item.position=new BABYLON.Vector3(item_position.x, item_position.y, item_position.z);
+	item.checkCollisions= true;
+	if (material!=  undefined){
+		item.material=material;
+	}
+	return item
+}
+
+function populate_template(config_file, room_name,scene){
+	// init
+	
+	//customizable parameters of the construction
+	let door_height=3;		// dimensions of the door to the parent gallery
+	let door_width=2;
+	let item_separation=0.1; //this prevents that items and walls are co-planar		
+	let item_size=2;		 //parameter controlling the scale of the items
+	
+	const vector_n=new BABYLON.Vector3(0, 0, -1);
+	const vector_s=new BABYLON.Vector3(0, 0, 1);
+	const vector_e=new BABYLON.Vector3(1, 0, 0);
+	const vector_w=new BABYLON.Vector3(-1, 0, 0);
+	
+	let W=config_file[room_name]["geometry"][0];
+	let L=config_file[room_name]["geometry"][1];
+	let H=config_file[room_name]["geometry"][2];
+
+
+	let item_vposition= 1+item_size/2; // vertical position
+
+	let NN=config_file[room_name]["geometry"][3];
+	let NS=config_file[room_name]["geometry"][4];
+	let NW=config_file[room_name]["geometry"][5];
+	let NE=config_file[room_name]["geometry"][6];
+
+	with_door=true;
+	var items_material=new Array(NN+NS+NW+NE);
+	var item_names=new Array(NN+NS+NW+NE);
+	var item_width=new Array(NN+NS+NW+NE);
+	var item_height=new Array(NN+NS+NW+NE);
+	
+	const dict_items=Object.keys(config_file[room_name]);
+	
+	//the root gallery has some differences with any other gallery
+	for (k=2; k<dict_items.length; k++){
+		items_material[k-2]=new BABYLON.StandardMaterial("item_mat"+k);
+		if (room_name=='root'){
+			//only doors
+			let tex=new BABYLON.DynamicTexture("DynamicTexture", {width:500, height:300}, scene, false);
+			items_material[k-2].diffuseTexture=tex;
+			tex.drawText(dict_items[k], null, null, font, "#000000", "#ffffff", true);
+			item_vposition=door_height/2;
+			item_names[k-2]="d_" + dict_items[k] + "_";
+			with_door=false;
+		} else {
+			
+			let tex=new BABYLON.Texture(hallspics_prefix + config_file[room_name][dict_items[k]]["resource"]);
+			items_material[k-2].diffuseTexture=tex;
+			item_names[k-2]=dict_items[k] + "_" + (k-2);
+			item_mesh=scene.getMeshByName(item_names[k-2]);
+			item_mesh.material=items_material[k-2];
+		}
+		item_width[k-2]=config_file[room_name][dict_items[k]]["width"];
+		item_height[k-2]=config_file[room_name][dict_items[k]]["height"];
+		
+
+	}
+		
+
+	const regex = /_\d{1,2}$/;
+
+	// console.log(scene.meshes);
+	// console.log(item_names);
+	for (var i in scene.meshes) {
+		scene.meshes[i].checkCollisions = true;
+			// if (regex.test(scene.meshes[i].name)) {
+				// scene.meshes[i].material=items_material[i];
+			// }
+	}
+	
+	//door
+	//door material
+	var root_doorMaterial = new BABYLON.StandardMaterial("doorMaterial", scene);
+	var font = "bold " + 48 + "px Arial";
+
+	root_door_tex=new BABYLON.DynamicTexture("DynamicTexture", {width:500, height:300}, scene, false);
+	root_doorMaterial.diffuseTexture = root_door_tex;
+	root_door_tex.drawText("Entrance", null, null, font, "#000000", "#ffffff", true);
+	door=item_builder("d_root",{x:0, y:door_height/2, z:L/2-item_separation}, {width:door_width, height:door_height}, vector_n, root_doorMaterial, scene);
+}	
+
 
 function rb(config_file, room_name, scene) {
 
@@ -52,8 +145,8 @@ function rb(config_file, room_name, scene) {
 		var concrete = new BABYLON.StandardMaterial("wall");
 		const concrete_tex = new BABYLON.Texture(materials_folder + "/concrete/gravel_concrete_diff_1k.jpg");
 		const concrete_tex2=new BABYLON.Texture(materials_folder + "/concrete/gravel_concrete_nor_gl_1k.jpg");
-		concrete_tex.uScale=15;
-		concrete_tex.vScale=5;
+		concrete_tex.uScale=5;
+		concrete_tex.vScale=1;
 		concrete.diffuseTexture= concrete_tex;
 		
 		//door material
@@ -68,6 +161,7 @@ function rb(config_file, room_name, scene) {
 		const dict_items=Object.keys(config_file[room_name]);
 		
 		//the root gallery has some differences with any other gallery
+		//k=0 and 1 are the geometry and parent.
 		for (k=2; k<dict_items.length; k++){
 			items_material[k-2]=new BABYLON.StandardMaterial("item_mat"+k);
 			if (room_name=='root'){
@@ -89,16 +183,7 @@ function rb(config_file, room_name, scene) {
 
 		}
         
-        var item_builder= function(name, item_position, item_size, vector, material){
-            var abstractPlane = BABYLON.Plane.FromPositionAndNormal(new BABYLON.Vector3(0, 0, 0),vector );
-            var item = BABYLON.MeshBuilder.CreatePlane(name, {sourcePlane: abstractPlane, width:item_size.width, height: item_size.height, sideOrientation: BABYLON.Mesh.SINGLESIDE},scene);
-            item.position=new BABYLON.Vector3(item_position.x, item_position.y, item_position.z);
-			item.checkCollisions= true;
-			if (material!=  undefined){
-				item.material=material;
-			}
-            return item
-        }
+
         
 		
 		
@@ -108,19 +193,19 @@ function rb(config_file, room_name, scene) {
 		floor.checkCollisions=true;
 
         //create north wall
-        let north_wall=item_builder("wall_n",{x:0, y:H/2, z:L/2}, {width:W, height:H}, vector_n, concrete);
+        let north_wall=item_builder("wall_n",{x:0, y:H/2, z:L/2}, {width:W, height:H}, vector_n, concrete,scene);
         
         //crate south wall
-        let south_wall=item_builder("wall_s",{x:0, y:H/2, z:-L/2}, {width:W, height:H}, vector_s, concrete);
+        let south_wall=item_builder("wall_s",{x:0, y:H/2, z:-L/2}, {width:W, height:H}, vector_s, concrete,scene);
 
         //crate east wall
-        let east_wall=item_builder("wall_e",{x:-W/2, y:H/2, z:0}, {width:L, height:H}, vector_e, concrete);
+        let east_wall=item_builder("wall_e",{x:-W/2, y:H/2, z:0}, {width:L, height:H}, vector_e, concrete,scene);
 
         //crate west wall
-        let west_wall=item_builder("wall_w",{x:W/2, y:H/2, z:0}, {width:L, height:H}, vector_w, concrete);
+        let west_wall=item_builder("wall_w",{x:W/2, y:H/2, z:0}, {width:L, height:H}, vector_w, concrete,scene);
        
         if (with_door){
-            door=item_builder("d_root",{x:0, y:door_height/2, z:L/2-item_separation}, {width:door_width, height:door_height}, vector_n, root_doorMaterial);
+            door=item_builder("d_root",{x:0, y:door_height/2, z:L/2-item_separation}, {width:door_width, height:door_height}, vector_n, root_doorMaterial,scene);
         }
 
 
@@ -138,14 +223,14 @@ function rb(config_file, room_name, scene) {
                 for (i=1; i<=NL; i++){
                     //place left
                     delta=(W/2-door_width/2)/(NL+1);
-                    item=item_builder(item_names[j] + j,{x:-W/2+delta*i, y:item_vposition, z:L/2-item_separation}, {width:item_size*item_width[j], height:item_size*item_height[j]}, vector_n, items_material[j]);
+                    item=item_builder(item_names[j] + j,{x:-W/2+delta*i, y:item_vposition, z:L/2-item_separation}, {width:item_size*item_width[j], height:item_size*item_height[j]}, vector_n, items_material[j], scene);
                     j++;
                 }
 
                 for (i=1; i<=NR; i++){
                     //place right
                     delta=(W/2-door_width/2)/(NR+1);
-                    item=item_builder(item_names[j] + j,{x:W/2-delta*i, y:item_vposition, z:L/2-item_separation}, {width:item_size*item_width[j], height:item_size*item_height[j]}, vector_n, items_material[j]);                    
+                    item=item_builder(item_names[j] + j,{x:W/2-delta*i, y:item_vposition, z:L/2-item_separation}, {width:item_size*item_width[j], height:item_size*item_height[j]}, vector_n, items_material[j], scene);                    
                     j++;
                 }
 
@@ -153,7 +238,7 @@ function rb(config_file, room_name, scene) {
                     for (i=1; i<=NN; i++){
                         //place north
                         delta=W/(NN+1);
-                        item=item_builder(item_names[j] + j,{x:-W/2+delta*i, y:item_vposition, z:L/2-item_separation}, {width:item_size*item_width[j], height:item_size*item_height[j]}, vector_n, items_material[j]); 
+                        item=item_builder(item_names[j] + j,{x:-W/2+delta*i, y:item_vposition, z:L/2-item_separation}, {width:item_size*item_width[j], height:item_size*item_height[j]}, vector_n, items_material[j], scene); 
                         j++;
                     }
 
@@ -165,7 +250,7 @@ function rb(config_file, room_name, scene) {
         for (i=1; i<=NS; i++){
             //place south
             delta=W/(NS+1);
-            item=item_builder(item_names[j] + j,{x:-W/2+delta*i, y:item_vposition, z:-L/2+item_separation}, {width:item_size*item_width[j], height:item_size*item_height[j]}, vector_s, items_material[j]); 
+            item=item_builder(item_names[j] + j,{x:-W/2+delta*i, y:item_vposition, z:-L/2+item_separation}, {width:item_size*item_width[j], height:item_size*item_height[j]}, vector_s, items_material[j], scene); 
             j++;
         }        
 
@@ -173,14 +258,14 @@ function rb(config_file, room_name, scene) {
         for (i=1; i<=NE; i++){
             //place east
             delta=L/(NE+1);
-            item=item_builder(item_names[j] + j,{x:-W/2+item_separation, y:item_vposition, z:-L/2+delta*i}, {width:item_size*item_width[j], height:item_size*item_height[j]}, vector_e, items_material[j]); 
+            item=item_builder(item_names[j] + j,{x:-W/2+item_separation, y:item_vposition, z:-L/2+delta*i}, {width:item_size*item_width[j], height:item_size*item_height[j]}, vector_e, items_material[j], scene); 
             j++;
         }   
         //west
         for (i=1; i<=NW; i++){
             //place west
             delta=L/(NW+1);
-            item=item_builder(item_names[j] + j,{x:W/2-item_separation, y:item_vposition, z:-L/2+delta*i}, {width:item_size*item_width[j], height:item_size*item_height[j]}, vector_w, items_material[j]); 
+            item=item_builder(item_names[j] + j,{x:W/2-item_separation, y:item_vposition, z:-L/2+delta*i}, {width:item_size*item_width[j], height:item_size*item_height[j]}, vector_w, items_material[j], scene); 
             j++;
         }   
 		
