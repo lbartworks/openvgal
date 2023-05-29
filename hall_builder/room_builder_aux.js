f18cdd4ab0f741a9917b1f9223c73a272079b12c
LB artworks <lbartworks@protonmail.com>
2023-05-29 14:59:35 +0200

    intermediate version with materials from file

--- hall_builder/room_builder_aux.js
+++ hall_builder/room_builder_aux.js
@@ -9,13 +9,111 @@ async function  doDownload(filename, scene) {
 }
 
 
+async function read_styles (filename){
+
+
+	await fetch(filename)
+		.then(response => response.json())
+		.then(data => {
+			// create a placeholder for materials
+			const materials = {};
+
+			Object.keys(data.styles).forEach(key => {
+				data.styles[key].forEach(style => {
+					let material;
+
+					if(style.materialType === "PBR") {
+						material = new BABYLON.PBRMaterial(style.styleName, scene);
+						//material.specularColor=new BABYLON.Color3(0,0,0);
+						if (style.textureFiles) {
+							if (style.textureFiles.bump) {
+								material.bumpTexture = new BABYLON.Texture(materials_folder + style.textureFiles.bump, scene);
+							}
+							if (style.textureFiles.diffuse) {
+								material.albedoTexture = new BABYLON.Texture(materials_folder +style.textureFiles.diffuse, scene);
+							}
+							if (style.textureFiles.roughness) {
+								material.metallicTexture = new BABYLON.Texture(materials_folder +style.textureFiles.roughness, scene);
+							}
+						}
+						if (style.color) {
+							material.albedoColor = new BABYLON.Color3.FromHexString(style.color);
+							material.reflectivityColor=new BABYLON.Color3(0,0,0);
+						}
+					} else {
+						material = new BABYLON.StandardMaterial(style.styleName, scene);
+						material.specularColor=new BABYLON.Color3(0,0,0);
+						if (style.color) {
+							material.diffuseColor = new BABYLON.Color3.FromHexString(style.color);
+							material.reflectivityColor=new BABYLON.Color3(0,0,0);
+						}
+					}
+					if (style.uvScaling) {
+						material.uScale = style.uvScaling.uScale;
+						material.vScale = style.uvScaling.vScale;
+					}
+					materials[style.styleName] = material;
+				});
+			});
+
+			// Apply the chosen styles to global variables
+			// I'm assuming that the chosen styles in the JSON file match the names of the global variables.
+			door_material = materials[data.chosenStyles.chosenDoorStyle];
+			wall_material = materials[data.chosenStyles.chosenWallStyle];
+			floor_material = materials[data.chosenStyles.chosenFloorStyle];
+			header_material = materials[data.chosenStyles.chosenHeaderStyle];
+
+			// add checks to ensure materials were assigned correctly
+			if (!door_material) {
+				console.warn(`Style ${data.chosenStyles.chosenDoorStyle} not found in materials.`);
+			}
+			if (!wall_material) {
+				console.warn(`Style ${data.chosenStyles.chosenWallStyle} not found in materials.`);
+			}
+			if (!floor_material) {
+				console.warn(`Style ${data.chosenStyles.chosenFloorStyle} not found in materials.`);
+			}
+			if (!header_material) {
+				console.warn(`Style ${data.chosenStyles.chosenHeaderStyle} not found in materials.`);
+			}
+		});
+
+}
+
 var item_builder= function(name, item_position, item_size, vector, material,scene){
-	var abstractPlane = BABYLON.Plane.FromPositionAndNormal(new BABYLON.Vector3(0, 0, 0),vector );
+	var base_vector=new BABYLON.Vector3(0, 0, 0);
+	var abstractPlane = BABYLON.Plane.FromPositionAndNormal(base_vector,vector );
 	var item = BABYLON.MeshBuilder.CreatePlane(name, {sourcePlane: abstractPlane, width:item_size.width, height: item_size.height, sideOrientation: BABYLON.Mesh.SINGLESIDE},scene);
 	item.position=new BABYLON.Vector3(item_position.x, item_position.y, item_position.z);
 	item.checkCollisions= true;
 	if (material!=  undefined){
 		item.material=material;
+		item.material.specularColor=new BABYLON.Color3(0,0,0);
+	}
+	if (name.startsWith("d_")){
+		//create 3d text
+		if (name.toLowerCase() =="d_root"){
+			texto='Entrance';
+		} else {
+			texto=name.replace(/d_(.+)_\d+/, "$1");;
+		}
+		myText = BABYLON.MeshBuilder.CreateText("T_" + texto, texto, fontContent, {
+			size: 0.2,
+			resolution: 5, 
+			depth: 0.1,
+			sideOrientation:2 }, scene);
+
+		//place it
+		myText.position=new BABYLON.Vector3(item_position.x, item_position.y, item_position.z);
+		
+		//rotare
+		let angleRad = Math.atan2(vector.x, vector.z) + Math.PI;
+		myText.rotation.y=angleRad;
+		
+		//assign material
+		myText.material = header_material;
+		// myText.material.albedoColor=new BABYLON.Color3(0.3,0.3,0.3);
+		// myText.material.reflectivityColor=new BABYLON.Color3(0,0,0);
 	}
 	return item
 }
@@ -57,6 +155,7 @@ function populate_template(config_file, room_name,scene){
 	//the root gallery has some differences with any other gallery
 	for (k=2; k<dict_items.length; k++){
 		items_material[k-2]=new BABYLON.StandardMaterial("item_mat"+k);
+		items_material[k-2].specularColor=new BABYLON.Color3(0,0,0);
 		if (room_name=='root'){
 			//only doors
 			let tex=new BABYLON.DynamicTexture("DynamicTexture", {width:500, height:300}, scene, false);
@@ -109,12 +208,13 @@ function rb(config_file, room_name, scene) {
 		let door_height=3;		// dimensions of the door to the parent gallery
         let door_width=2;
 		let item_separation=0.1; //this prevents that items and walls are co-planar		
-        let item_size=2;		 //parameter controlling the scale of the items
+        let item_size=3;		 //parameter controlling the scale of the items
 		
 		const vector_n=new BABYLON.Vector3(0, 0, -1);
         const vector_s=new BABYLON.Vector3(0, 0, 1);
         const vector_e=new BABYLON.Vector3(1, 0, 0);
         const vector_w=new BABYLON.Vector3(-1, 0, 0);
+		const vector_d=new BABYLON.Vector3(0,-1,0);
         
 		let W=config_file[room_name]["geometry"][0];
 		let L=config_file[room_name]["geometry"][1];
@@ -135,27 +235,15 @@ function rb(config_file, room_name, scene) {
 		var item_height=new Array(NN+NS+NW+NE);
 		
 		//floor material
-		var wood = new BABYLON.StandardMaterial("wood");
-		var wood_tex=new BABYLON.Texture(materials_folder + "/wood_floor/WoodFloor051_2K_Color.jpg");
-		wood_tex.uScale=5;
-		wood_tex.vScale=5;
-		wood.diffuseTexture = wood_tex;
+		var wood = floor_material;
+		console.log(floor_material);
+
 		
 		//wall material
-		var concrete = new BABYLON.StandardMaterial("wall");
-		const concrete_tex = new BABYLON.Texture(materials_folder + "/concrete/gravel_concrete_diff_1k.jpg");
-		const concrete_tex2=new BABYLON.Texture(materials_folder + "/concrete/gravel_concrete_nor_gl_1k.jpg");
-		concrete_tex.uScale=5;
-		concrete_tex.vScale=1;
-		concrete.diffuseTexture= concrete_tex;
-		
-		//door material
-		var root_doorMaterial = new BABYLON.StandardMaterial("doorMaterial", scene);
-		var font = "bold " + 48 + "px Arial";
+		var concrete = wall_material;
 
-		root_door_tex=new BABYLON.DynamicTexture("DynamicTexture", {width:500, height:300}, scene, false);
-        root_doorMaterial.diffuseTexture = root_door_tex;
-		root_door_tex.drawText("Entrance", null, null, font, "#000000", "#ffffff", true);
+			//door material
+		var root_doorMaterial = door_material;
 
 
 		const dict_items=Object.keys(config_file[room_name]);
@@ -163,16 +251,14 @@ function rb(config_file, room_name, scene) {
 		//the root gallery has some differences with any other gallery
 		//k=0 and 1 are the geometry and parent.
 		for (k=2; k<dict_items.length; k++){
-			items_material[k-2]=new BABYLON.StandardMaterial("item_mat"+k);
+			
 			if (room_name=='root'){
-				let tex=new BABYLON.DynamicTexture("DynamicTexture", {width:500, height:300}, scene, false);
-				items_material[k-2].diffuseTexture=tex;
-				tex.drawText(dict_items[k], null, null, font, "#000000", "#ffffff", true);
+				items_material[k-2]=root_doorMaterial;
 				item_vposition=door_height/2;
 				item_names[k-2]="d_" + dict_items[k] + "_";
 				with_door=false;
 			} else {
-				
+				items_material[k-2]=new BABYLON.StandardMaterial("item_mat"+k);
 				let tex=new BABYLON.Texture(hallspics_prefix + config_file[room_name][dict_items[k]]["resource"]);
 				items_material[k-2].diffuseTexture=tex;
 				item_names[k-2]=dict_items[k] + "_";
@@ -183,14 +269,16 @@ function rb(config_file, room_name, scene) {
 
 		}
         
-
-        
-		
-		
+	
         //create floor
         var floor= BABYLON.MeshBuilder.CreateGround("floor", {width: W, height:L}, scene);
 		floor.material=wood;
 		floor.checkCollisions=true;
+		
+		//create ceiling
+		if (with_ceiling) {
+			let ceiling=item_builder("ceiling",{x:0, y:H, z:0}, {width:L, height:W}, vector_d, concrete,scene);
+		}
 
         //create north wall
         let north_wall=item_builder("wall_n",{x:0, y:H/2, z:L/2}, {width:W, height:H}, vector_n, concrete,scene);
@@ -206,6 +294,7 @@ function rb(config_file, room_name, scene) {
        
         if (with_door){
             door=item_builder("d_root",{x:0, y:door_height/2, z:L/2-item_separation}, {width:door_width, height:door_height}, vector_n, root_doorMaterial,scene);
+
         }
 
 
